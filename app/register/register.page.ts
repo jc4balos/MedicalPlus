6c7abeb1ccf3fc1f6d9dca9181ea98aeaecb04e5
PDG <pumpkinwarrior22@gmail.com>
2022-12-08 11:51:57 +0800

    merged with kathlyn

--- app/register/register.page.ts
+++ app/register/register.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
-  styleUrls: ['../login/login.page.scss'],
+  styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
 
